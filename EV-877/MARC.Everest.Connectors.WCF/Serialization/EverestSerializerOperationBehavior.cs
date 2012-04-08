5bf55bd8539d416ab97e2223935ec8fd70ff69b5
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2012-04-08 17:52:43 +0000

    Resolves EV-878
    git-svn-id: https://fisheye.marc-hi.ca/svn/everest/branches/1.1@18 0a814079-c22b-1640-8a39-4bdcdc761e1f

---
+++ EV-877/MARC.Everest.Connectors.WCF/Serialization/EverestSerializerOperationBehavior.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel.Description;
+
+namespace MARC.Everest.Connectors.WCF.Serialization
+{
+    
+    /// <summary>
+    /// Everest serializer operation behavior
+    /// </summary> 
+    public class EverestSerializerOperationBehavior : DataContractSerializerOperationBehavior
+    {
+
+        // The formatter
+        private IXmlStructureFormatter m_formatter;
+
+        /// <summary>
+        /// Constructs a new instance of the Everest Serializer operation behavior
+        /// </summary>
+        public EverestSerializerOperationBehavior(OperationDescription operation, IXmlStructureFormatter formatter) : base(operation)
+        {
+            // TODO: Complete member initialization
+            this.m_formatter = formatter;
+        }
+
+        /// <summary>
+        /// Create a serializer
+        /// </summary>
+        public override System.Runtime.Serialization.XmlObjectSerializer CreateSerializer(Type type, string name, string ns, IList<Type> knownTypes)
+        {
+            return new XmlSerializerSurrogate(this.m_formatter);
+        }
+
+        /// <summary>
+        /// Create the serializer
+        /// </summary>
+        public override System.Runtime.Serialization.XmlObjectSerializer CreateSerializer(Type type, System.Xml.XmlDictionaryString name, System.Xml.XmlDictionaryString ns, IList<Type> knownTypes)
+        {
+            return new XmlSerializerSurrogate(this.m_formatter);
+        }
+    }
+}
