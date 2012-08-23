98761bed1d145abdfac8dd183207651004d5d4bc
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2012-08-23 00:06:49 +0000

    Update samples
    git-svn-id: https://fisheye.marc-hi.ca/svn/everest/branches/1.0@63 0a814079-c22b-1640-8a39-4bdcdc761e1f

--- Samples/Guide/Samples.Everest.ClinicalDocuments.CdaConstruction/Program.cs
+++ Samples/Guide/Samples.Everest.ClinicalDocuments.CdaConstruction/Program.cs
@@ -44,11 +44,10 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
 
             // Create the CDA
             ClinicalDocument cda = new ClinicalDocument(
-                ActClassClinicalDocument.DOCCLIN, // Document type is clinical document
                 new II("2.16.840.1.113883.19.4", "c266"), // Create an identifier for the document
-                new CE<DocumentType>(DocumentType._ActIdentityDocumentCode, "2.16.840.1.113883.6.1", "LOINC", null, "Consultation note", null), // Specify the type of document
+                new CE<String>("03094", "2.16.840.1.113883.6.1", "LOINC", null, "Consultation note", null), // Specify the type of document
                 DateTime.Now, // Effective time of the document (now)
-                x_BasicConfidentialityKind.N_, // Confidentiality code of N = Normal
+                x_BasicConfidentialityKind.Normal, // Confidentiality code of N = Normal
                 CreateRecordTarget(), // Create a record target, this is good for code reuse
                 CreateAuthor(), // Create the author node
                 CreateCustodian(), // Create custodian node
@@ -78,7 +77,7 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
                 xsw.Flush();
                 Console.WriteLine("Render #{1} Took {0} ms to render", DateTime.Now.Subtract(start).TotalMilliseconds, i);
             }
-            
+
             Console.ReadKey();
         }
 
@@ -90,12 +89,12 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
         {
             Console.Write(prompt);
             string response = default(string);
-            while(true)
+            while (true)
             {
                 response = Console.ReadLine();
                 if (String.IsNullOrEmpty(response))
                     Console.Write("Invalid response! {0}", prompt);
-                else 
+                else
                     break;
             }
             return new T() { Value = response };
@@ -108,20 +107,18 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
         {
             // Create a component 2 structure, which contains the body of the CDA, set it's type code to COMP and
             // context conduction indicator (any data with context conduction code of AP will be conducted) to true
-            var retVal = new Component2(ActRelationshipHasComponent.COMP, true);
-            
+            var retVal = new Component2(ActRelationshipHasComponent.HasComponent, true);
+
 
             // Everest provides methods of creating abstract classes through a series of helper methods,
             // here we're creating an observation for asthma
             var asthma = ClinicalStatement.CreateObservation(
-                ActClassObservation.COND,
-                x_ActMoodDocumentObservation.EVN,
-                new CD<ObservationType>(ObservationType._PatientCharacteristicObservationType, "2.16.840.1.113883.6.96", "SNOMED-CT", null, "Asthma", null)
+                x_ActMoodDocumentObservation.Eventoccurrence,
+                new CD<String>("23433303", "2.16.840.1.113883.6.96", "SNOMED-CT", null, "Asthma", null)
             );
             asthma.StatusCode = ActStatus.Completed;
             asthma.EffectiveTime = new IVL<TS>(new TS(DateTime.Now, DatePrecision.Year), null); // In Everest, we can create a date with a precision of year which can be interpreted as any time that year
-            asthma.Reference.Add(new Reference(x_ActRelationshipExternalReference.XCRPT, ExternalActChoice.CreateExternalObservation(ActClassObservation.COND)));
-            
+
             // BodyChoice is a choice of either nonXml content or structured body, how would we know that?
             // Well, whenever a class as a property XChoice and SetXChoice the SetXChoice will contain
             // the options of what XChoice can be set to
@@ -130,26 +127,26 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
                 Component = new List<Component3>() // We can use type initializers to create the components (sections)
                 {
                     // We can create a section for history of present illness
-                    new Component3(ActRelationshipHasComponent.COMP, true, 
+                    new Component3(ActRelationshipHasComponent.HasComponent, true, 
                         new Section() {
                             Code = new CE<string>("10164-2", "2.16.840.1.113883.6.1"),
                             Title = "History of Present Illness",
                             Text = new ED("<content styleCode=\"Bold\">This is bold</content>") { Representation = EncapsulatedDataRepresentation.XML } // In the CDA ED type, we can use content tags to style the content
                         }),
-                    new Component3(ActRelationshipHasComponent.COMP, true,
+                    new Component3(ActRelationshipHasComponent.HasComponent, true,
                         new Section() {
                             Code = new CE<string>("10153-2", "2.16.840.1.113883.6.1"),
                             Title = "Past Medical History",
                             Text = "We can also assign an ED directly as a string",
                             Entry = new List<Entry>() {  // We can also add observations and sub-components
-                                new Entry(x_ActRelationshipEntry.COMP, true, asthma) // See where we defined astma above as a clinical statement
+                                new Entry(x_ActRelationshipEntry.HasComponent, true, asthma) // See where we defined astma above as a clinical statement
                             }
                         })
                 }
             });
 
             return retVal;
-            
+
         }
 
         /// <summary>
@@ -160,7 +157,6 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
             //throw new NotImplementedException();
             return new Custodian(
                 new AssignedCustodian(
-                    RoleClassAssignedEntity.ASSIGNED
                 )
             );
         }
@@ -174,7 +170,6 @@ namespace Samples.Everest.ClinicalDocuments.CdaConstruction
                 ContextControl.AdditivePropagating,
                 DateTime.Now,
                 new AssignedAuthor(
-                    RoleClassAssignedEntity.ASSIGNED,
                     new SET<II>(new II("1.2.3.4.5.6", "1234"))
                 )
             );
