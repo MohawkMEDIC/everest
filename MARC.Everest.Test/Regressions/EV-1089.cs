8f7a2761028a9233c8fe6f8de5dce21199e277a8
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2013-06-06 15:53:01 +0000

    Updated test for EV-1089
    git-svn-id: https://fisheye.marc-hi.ca/svn/everest/branches/1.2@218 0a814079-c22b-1640-8a39-4bdcdc761e1f

---
+++ MARC.Everest.Test/Regressions/EV-1089.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MARC.Everest.DataTypes;
+using System.IO;
+using MARC.Everest.Formatters.XML.Datatypes.R1;
+using MARC.Everest.Xml;
+using System.Xml;
+
+namespace MARC.Everest.Test.Regressions
+{
+    [TestClass]
+    public class EV_1089
+    {
+        /// <summary>
+        /// Test parsing of ENXP
+        /// </summary>
+        [TestMethod]
+        public void EV_1089ENXPParseTest()
+        {
+            EN myEn = EN.FromFamilyGiven(EntityNameUse.License, "Toet", "J");
+            myEn.Part[0].Qualifier = new SET<CS<EntityNamePartQualifier>>();
+            myEn.Part[1].Qualifier = new SET<CS<EntityNamePartQualifier>>();
+            myEn.Part[0].Qualifier.Add(EntityNamePartQualifier.Birth);
+            myEn.Part[1].Qualifier.Add(EntityNamePartQualifier.Initial);
+            String xmlString = R1SerializationHelper.SerializeAsString(myEn);
+
+            StringReader sr = new StringReader(xmlString);
+            DatatypeFormatter fmtr = new DatatypeFormatter();
+            XmlStateReader rdr = new XmlStateReader(XmlReader.Create(sr));
+            while (rdr.NodeType != XmlNodeType.Element)
+                rdr.Read();
+            var result = fmtr.Parse(rdr, typeof(EN));
+            Assert.AreEqual(0, result.Details.Count(o=>o.Type == Connectors.ResultDetailType.Error));
+
+        }
+    }
+}
